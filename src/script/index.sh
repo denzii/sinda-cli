ee3cddd5d85bab3924cd7a84469896fa700971be
denizarca <denizarca1@gmail.com>
2022-03-23 14:07:49 +0200

    unix azure cli install script

--- src/script/index.sh
+++ src/script/index.sh
@@ -23,3 +23,26 @@ install_ohmyzsh() {
     sudo apt install git-core curl fonts-powerline
     sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 }
+
+install_azurecli(){
+    # commands put together from
+    # https://docs.microsoft.com/en-us/cli/azure/install-azure-cli-linux?pivots=apt
+
+    # Get packages needed for install
+    sudo apt-get update
+    sudo apt-get install ca-certificates curl apt-transport-https lsb-release gnupg
+
+    # Download and install the Microsoft signing key
+    curl -sL https://packages.microsoft.com/keys/microsoft.asc |
+        gpg --dearmor |
+        sudo tee /etc/apt/trusted.gpg.d/microsoft.gpg > /dev/null
+
+    # Add the Azure CLI software repository
+    AZ_REPO=$(lsb_release -cs)
+    echo "deb [arch=amd64] https://packages.microsoft.com/repos/azure-cli/ $AZ_REPO main" |
+        sudo tee /etc/apt/sources.list.d/azure-cli.list
+
+    # Update repository information and install the azure-cli package
+    sudo apt-get update
+    sudo apt-get install azure-cli
+}
