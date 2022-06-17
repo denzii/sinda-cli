da1cc0836ecef8eaa16155174cd474aef391df2e
denzii <denizarca1@gmail.com>
2022-06-17 21:14:04 +0300

    Update entry-win.ps1

--- src/script/entry-win.ps1
+++ src/script/entry-win.ps1
@@ -1,10 +1,1 @@
-
-function Invoke-Script {
-    param ( $TempCygDir="$env:temp\cygInstall" )
-    
-    wget "https://cdn.githubraw.com/denzii/sinda-cli/main/index.json" -outfile "onlineScript.ps1"
-}
-
-Invoke-Script
-
-Get-Content -Path ".\onlineScript.ps1"
+echo hi
